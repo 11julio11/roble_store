6844eff65941286b89005009c88727d45c7c36f8
David11 <romerojesusdavid76@gmail.com>
2023-11-14 20:02:47 -0500

    adaptacion de carrusel automatico

--- scripts.js
+++ scripts.js
@@ -295,4 +295,35 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-//tipos de muebles
+
+
+//funconamiemto del carrusel automatico
+const carrusel = document.querySelector(".carrusel-items");
+
+let maxScrollLeft = carrusel.scrollWidth - carrusel.clientWidth;
+let intervalo = null;
+let step = 2;
+const start = () => {
+  intervalo = setInterval(function () {
+    carrusel.scrollLeft = carrusel.scrollLeft + step;
+    if (carrusel.scrollLeft === maxScrollLeft) {
+      step = step * -1;
+    } else if (carrusel.scrollLeft === 0) {
+      step = step * -1;
+    }
+  }, 10);
+};
+
+const stop = () => {
+  clearInterval(intervalo);
+};
+
+carrusel.addEventListener("mouseover", () => {
+  stop();
+});
+
+carrusel.addEventListener("mouseout", () => {
+  start();
+});
+
+start();
