4cae3a9e9cc26e50d2864532552a04a86cf89f04
David11 <romerojesusdavid76@gmail.com>
2023-11-17 11:52:44 -0500

    eliminacion del carrusel 17/11/23

--- scripts.js
+++ scripts.js
@@ -298,32 +298,4 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 //funconamiemto del carrusel automatico
-const carrusel = document.querySelector(".carrusel-items");
 
-let maxScrollLeft = carrusel.scrollWidth - carrusel.clientWidth;
-let intervalo = null;
-let step = 2;
-const start = () => {
-  intervalo = setInterval(function () {
-    carrusel.scrollLeft = carrusel.scrollLeft + step;
-    if (carrusel.scrollLeft === maxScrollLeft) {
-      step = step * -1;
-    } else if (carrusel.scrollLeft === 0) {
-      step = step * -1;
-    }
-  }, 10);
-};
-
-const stop = () => {
-  clearInterval(intervalo);
-};
-
-carrusel.addEventListener("mouseover", () => {
-  stop();
-});
-
-carrusel.addEventListener("mouseout", () => {
-  start();
-});
-
-start();
